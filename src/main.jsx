f424ebf461512a34211eb20ba005dba05df89e89
Waysidechimp <67596048+Waysidechimp@users.noreply.github.com>
2026-03-12 17:34:07 -0400

    New build, hopefully a fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-const cors = require('cors')
-app.use(cors())
-
 createRoot(document.getElementById('root')).render(<App />)
